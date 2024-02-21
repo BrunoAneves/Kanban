2661d474e060963bec00b2540eb23ccdcdd0324d
Douglas Lima <douglas.lima.queiroz@gmail.com>
2024-02-21 18:23:22 -0300

    up- login and register

---
+++ src/components/Cadastro/index.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+
+export type User = {
+    id: number;
+    nome: string;
+    email: string;
+    senha: string;
+};
+
+const Cadastro: React.FC = () => {
+
+    const [userEmail, setUserEmail] = useState<string>('');
+    const [userNome, setUserNome] = useState<string>('');
+    const [userPassword, setUserPassword] = useState<string>('');
+
+    const handleRegister = () => {
+        const userData: User = {
+            id: Math.floor(Math.random() * 1000), // Gerando um ID aleatório para o usuário
+            nome: userNome,
+            email: userEmail,
+            senha: userPassword
+        };
+
+        localStorage.setItem('userData', JSON.stringify(userData));
+
+        setUserEmail('');
+        setUserNome('');
+        setUserPassword('');
+
+        console.log('Foi :', userData);
+    }
+
+    return (
+        <div>
+            <h2>Cadastro de Usuário</h2>
+            <form>
+                <div>
+                    <label htmlFor="nome">Nome:</label>
+                    <input
+                        type="text"
+                        id="nome"
+                        value={userNome}
+                        onChange={(e) => setUserNome(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={userEmail}
+                        onChange={(e) => setUserEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="senha">Senha:</label>
+                    <input
+                        type="password"
+                        id="senha"
+                        value={userPassword}
+                        onChange={(e) => setUserPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="button" onClick={handleRegister}>Cadastrar</button>
+            </form>
+        </div>
+    );
+}
+
+export default Cadastro;
---
+++ src/components/Login/index.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { User } from '../Cadastro';
+
+const Login: React.FC = () => {
+
+    const [userEmail, setUserEmail] = useState<string>('');
+    const [userPassword, setUserPassword] = useState<string>('');
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+
+    const handleLogin = () => {
+        const storedUserData: string | null = localStorage.getItem('userData');
+        if (storedUserData) {
+            const userData: User = JSON.parse(storedUserData);
+            if (userData.email === userEmail && userData.senha === userPassword) {
+                setIsLoggedIn(true);
+                console.log('Login feito!');
+            } else {
+                console.log('Email ou senha incorretos');
+            }
+        } else {
+            console.log('Tá louco? Cadastra aí primeiro.');
+        }
+    };
+
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+        console.log('Logout realizado.');
+    };
+
+    return (
+        <div>
+            {isLoggedIn ? (
+                <div>
+                    <h2>Usuário Logado</h2>
+                    <button onClick={handleLogout}>Logout</button>
+                </div>
+            ) : (
+                    <div>
+                        <h2>Login de Usuário</h2>
+                        <form>
+                            <div>
+                                <label htmlFor="email">Email:</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    value={userEmail}
+                                    onChange={(e) => setUserEmail(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="senha">Senha:</label>
+                                <input
+                                    type="password"
+                                    id="senha"
+                                    value={userPassword}
+                                    onChange={(e) => setUserPassword(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <button type="button" onClick={handleLogin}>Login</button>
+                        </form>
+                    </div>
+                )}
+        </div>
+    );
+}
+
+export default Login;
